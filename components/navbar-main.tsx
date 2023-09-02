78ec6790e296d9a5d253dddf56d2d46b66116b46
yasinadil <adilyasin205@gmail.com>
2023-09-02 17:30:51 +0500

    font changes i guess

--- components/navbar-main.tsx
+++ components/navbar-main.tsx
@@ -15,24 +15,21 @@ export default function NavbarMain() {
             priority={true}
           />
         </Link>
-        <div
-          className="flex flex-row gap-x-[60px]"
-          style={{ fontFamily: "Noto 400" }}
-        >
+        <div className="flex flex-row gap-x-[60px]">
           <a
-            className="hover:underline hover:underline-offset-8 hover:font-semibold text-right text-[19px] leading-[1.8rem]"
+            className="hover:underline hover:underline-offset-8 hover:font-medium  text-right text-[19px] leading-[1.8rem] "
             href="https://danglydoodads.wtf"
           >
             danglydoodads
           </a>
           <a
-            className="underline underline-offset-8 font-semibold text-right text-[19px] leading-[1.8rem]"
+            className="underline underline-offset-8 text-right font-medium text-[19px] leading-[1.8rem]"
             href="/"
           >
             mint.danglydoodads
           </a>
           <a
-            className="hover:underline hover:underline-offset-8 hover:font-semibold text-right text-[19px] leading-[1.8rem]"
+            className="hover:underline hover:underline-offset-8 hover:font-medium text-right text-[19px] leading-[1.8rem] "
             href="https://danglydoodads.wtf/other-schtuff"
           >
             other schtuff
